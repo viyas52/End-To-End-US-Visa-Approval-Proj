b25be5b8c23c3b4175d02d5dcccf2047d9e7c3fe
viyas52 <vyas.illusionist@gmail.com>
2024-07-31 19:21:56 +0530

    exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
-from us_visa.logger import logging
+from us_visa import logging
+from us_visa import CustomException
+import sys
 
-logging.info("welcome to our custom log")
+#logging.info("welcome to our custom log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise CustomException(e, sys)
---
+++ us_visa/__init__.py
@@ -0,0 +1,3 @@
+from us_visa.logger import logging
+from us_visa.exception import CustomException
+
