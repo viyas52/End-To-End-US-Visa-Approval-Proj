a3c646f59ab4cec53863532aca3a885baade0242
viyas52 <vyas.illusionist@gmail.com>
2024-08-07 13:24:24 +0530

    constants added

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,13 @@
+import os 
+from datetime import date
+
+DATABASE_NAME = "US_VISA"
+
+COLLECTION_NAME = "visa_data"
+
+MONGOBD_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
