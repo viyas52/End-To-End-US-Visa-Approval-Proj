eee2ee2d5e94ed2c8c68f944777fe079f944b7c2
viyas52 <vyas.illusionist@gmail.com>
2024-08-07 15:54:02 +0530

    entity added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -10,4 +10,20 @@ MONGOBD_URL_KEY = "MONGODB_URL"
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
 
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
+
+
+
+""" 
+DATA INGESTION related constants 
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
